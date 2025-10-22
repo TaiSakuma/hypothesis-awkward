01ee2a30b87e072e2d0a572b6e87e6589c347ea2
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-22 14:11:20 -0400

    Add `builtins_.py` and its tests

---
+++ src/hypothesis_awkward/builtins_.py
@@ -0,0 +1,122 @@
+import datetime
+from functools import partial
+from typing import TypeVar
+
+import numpy as np
+import pytest
+from hypothesis import Phase, given, note, settings
+from hypothesis import strategies as st
+from hypothesis.extra import numpy as st_np
+
+import awkward as ak
+
+T = TypeVar('T', bound=np.generic)
+
+BUILTIN_SAFE_DTYPE_NAMES = (
+    'bool',
+    'int64',
+    'float64',
+    'complex128',
+    'datetime64[us]',
+    'timedelta64[us]',
+)
+
+BUILTIN_SAFE_DTYPES = tuple(np.dtype(name) for name in BUILTIN_SAFE_DTYPE_NAMES)
+
+
+def builtin_safe_dtype_names() -> st.SearchStrategy[str]:
+    '''Strategy for names of NumPy dtypes with corresponding Python built-in types.
+
+    Examples
+    --------
+    >>> builtin_safe_dtype_names().example()
+    '...'
+    '''
+    return st.sampled_from(BUILTIN_SAFE_DTYPE_NAMES)
+
+
+def builtin_safe_dtypes() -> st.SearchStrategy[np.dtype]:
+    '''Strategy for NumPy dtypes with corresponding Python built-in types.
+
+    Examples
+    --------
+    >>> builtin_safe_dtypes().example()
+    dtype(...)
+    '''
+    return st.sampled_from(BUILTIN_SAFE_DTYPES)
+
+
+def items_from_dtype(
+    dtype: np.dtype[T], allow_nan: bool = False
+) -> st.SearchStrategy[T]:
+    '''Strategy for Python built-in type values for a given NumPy dtype.
+
+    Parameters
+    ----------
+    dtype
+        The NumPy dtype to generate items for.
+    allow_nan
+        Generate potentially `NaN` for relevant dtypes if `True`.
+
+    Examples
+    --------
+    >>> i = items_from_dtype(np.dtype('int64')).example()
+    >>> isinstance(i, int)
+    True
+    '''
+    return st_np.from_dtype(dtype, allow_nan=allow_nan).map(lambda x: x.item())
+
+
+@st.composite
+def lists(
+    draw: st.DrawFn, allow_nan: bool = False, max_size: int = 5, max_depth: int = 5
+) -> list:
+    '''Strategy for nested Python lists for which Awkward Arrays can be created.
+
+    Parameters
+    ----------
+    allow_nan
+        Generate potentially `NaN` for relevant dtypes if `True`.
+    max_size
+        Maximum list length at any depth.
+    max_depth
+        Maximum depth of nested lists.
+
+    Examples
+    --------
+    >>> lists().example()
+    [...]
+    '''
+
+    dtype = draw(builtin_safe_dtypes())
+    items = items_from_dtype(dtype, allow_nan=allow_nan)
+    extend = partial(st.lists, max_size=max_size)
+    max_leaves = max_depth - 1  # `-1` for the outermost `extend`
+    if max_leaves <= 0:
+        return draw(extend(items))
+    return draw(extend(st.recursive(base=items, extend=extend, max_leaves=max_leaves)))
+
+
+def from_list(
+    allow_nan: bool = False, max_size: int = 5, max_depth: int = 5
+) -> st.SearchStrategy[ak.Array]:
+    '''Strategy for Awkward Arrays created from Python lists.
+
+    Parameters
+    ----------
+    allow_nan
+        Generate potentially `NaN` for relevant dtypes if `True`.
+    max_size
+        Maximum list length at any depth.
+    max_depth
+        Maximum depth of nested lists.
+
+    Examples
+    --------
+    >>> from_list().example()
+    <Array ... type='...'>
+    '''
+    return st.builds(
+        ak.Array,
+        lists(allow_nan=allow_nan, max_size=max_size, max_depth=max_depth),
+    )
---
+++ tests/test_builtins_.py
@@ -0,0 +1,104 @@
+import math
+from typing import Any
+
+import numpy as np
+from hypothesis import given, note
+from hypothesis import strategies as st
+
+import awkward as ak
+from hypothesis_awkward.builtins_ import (
+    builtin_safe_dtypes,
+    from_list,
+    items_from_dtype,
+    lists,
+)
+
+
+@given(data=st.data())
+def test_items_from_dtype(data: st.DataObject) -> None:
+    dtype = data.draw(builtin_safe_dtypes(), label='dtype')
+    item = data.draw(items_from_dtype(dtype), label='item')
+
+    def _to_dtype_to_item(dtype: np.dtype, item: Any) -> Any:
+        if dtype.kind == 'M':  # datetime64
+            # datetime64 requires the unit.
+            unit, _ = np.datetime_data(dtype)
+            n = dtype.type(item, unit)
+        else:
+            n = dtype.type(item)
+        return n.item()
+
+    assert _to_dtype_to_item(dtype, item) == item
+
+
+@given(data=st.data())
+def test_lists(data: st.DataObject) -> None:
+    # Draw options
+    allow_nan = data.draw(st.booleans(), label='allow_nan')
+    max_size = data.draw(st.integers(min_value=0, max_value=5), label='max_size')
+    max_depth = data.draw(st.integers(min_value=1, max_value=5), label='max_depth')
+
+    # Call the test subject
+    l = data.draw(
+        lists(allow_nan=allow_nan, max_size=max_size, max_depth=max_depth), label='l'
+    )
+    assert isinstance(l, list)
+
+    # Assert the options were effective
+    def _is_nan(x: Any) -> bool:
+        if x is None:
+            # `NaT` becomes `None`
+            return True
+        elif isinstance(x, complex):
+            return math.isnan(x.real) or math.isnan(x.imag)
+        elif isinstance(x, float):
+            return math.isnan(x)
+        return False
+
+    def _examine(l: list) -> tuple[bool, int, int]:
+        '''Return (has_nan, max_size, depth) of the list `l`.'''
+        if not isinstance(l, list):
+            return (_is_nan(l), 0, 0)
+        has_nan = False
+        size = len(l)  # max length of lists
+        depth = 1
+        for item in l:
+            h, s, d = _examine(item)
+            has_nan = has_nan or h
+            size = max(size, s)
+            depth = max(depth, d + 1)
+        return (has_nan, size, depth)
+
+    has_nan, size, depth = _examine(l)
+    note(f'{has_nan=}, {size=}, {depth=}')
+
+    if not allow_nan:
+        assert not has_nan
+    assert size <= max_size
+    assert depth <= max_depth
+
+    # Assert an Awkward Array can be created.
+    a = ak.Array(l)
+    note(f'{a=}')
+    assert isinstance(a, ak.Array)
+
+    to_list = a.to_list()
+    note(f'{to_list=}')
+
+    if not has_nan:
+        assert to_list == l
+
+
+@given(data=st.data())
+def test_from_list(data: st.DataObject) -> None:
+    # Draw options
+    allow_nan = data.draw(st.booleans(), label='allow_nan')
+    max_size = data.draw(st.integers(min_value=0, max_value=5), label='max_size')
+    max_depth = data.draw(st.integers(min_value=1, max_value=5), label='max_depth')
+
+    # Call the test subject
+    a = data.draw(
+        from_list(allow_nan=allow_nan, max_size=max_size, max_depth=max_depth),
+        label='a',
+    )
+    assert isinstance(a, ak.Array)
