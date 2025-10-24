c89bccf79d3c7c546d713c6e0bdf2b91da7c3e43
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-24 13:53:54 -0400

    Update tests

--- tests/test_builtins_.py
+++ tests/test_builtins_.py
@@ -1,5 +1,5 @@
 import math
-from typing import Any
+from typing import Any, TypedDict
 
 import numpy as np
 from hypothesis import given, note
@@ -32,20 +32,52 @@ def test_items_from_dtype(data: st.DataObject) -> None:
     assert _to_dtype_to_item(dtype, item) == item
 
 
+class ListsKwargs(TypedDict, total=False):
+    dtype: np.dtype | st.SearchStrategy[np.dtype] | None
+    allow_nan: bool
+    max_size: int
+    max_depth: int
+
+
+@st.composite
+def lists_kwargs(draw: st.DrawFn) -> ListsKwargs:
+    kwargs = ListsKwargs()
+
+    if draw(st.booleans()):
+        kwargs['dtype'] = draw(
+            st.one_of(
+                st.none(),
+                st.just(builtin_safe_dtypes()),
+                builtin_safe_dtypes(),
+            )
+        )
+
+    if draw(st.booleans()):
+        kwargs['allow_nan'] = draw(st.booleans())
+
+    if draw(st.booleans()):
+        kwargs['max_size'] = draw(st.integers(min_value=0, max_value=5))
+
+    if draw(st.booleans()):
+        kwargs['max_depth'] = draw(st.integers(min_value=1, max_value=5))
+
+    return kwargs
+
+
 @given(data=st.data())
 def test_lists(data: st.DataObject) -> None:
     # Draw options
-    allow_nan = data.draw(st.booleans(), label='allow_nan')
-    max_size = data.draw(st.integers(min_value=0, max_value=5), label='max_size')
-    max_depth = data.draw(st.integers(min_value=1, max_value=5), label='max_depth')
+    kwargs = data.draw(lists_kwargs(), label='kwargs')
 
     # Call the test subject
-    l = data.draw(
-        lists(allow_nan=allow_nan, max_size=max_size, max_depth=max_depth), label='l'
-    )
-    assert isinstance(l, list)
+    l = data.draw(lists(**kwargs), label='l')
 
     # Assert the options were effective
+    dtype = kwargs.get('dtype', None)
+    allow_nan = kwargs.get('allow_nan', False)
+    max_size = kwargs.get('max_size', 5)
+    max_depth = kwargs.get('max_depth', 5)
+
     def _is_nan(x: Any) -> bool:
         if x is None:
             # `NaT` becomes `None`
@@ -56,22 +88,33 @@ def test_lists(data: st.DataObject) -> None:
             return math.isnan(x)
         return False
 
-    def _examine(l: list) -> tuple[bool, int, int]:
-        '''Return (has_nan, max_size, depth) of the list `l`.'''
+    def _examine(l: list) -> tuple[bool, int, int, set[type]]:
+        '''Return (has_nan, max_size, depth, types) of the list `l`.'''
         if not isinstance(l, list):
-            return (_is_nan(l), 0, 0)
+            is_nan = _is_nan(l)
+            type_l = {type(l)} if not is_nan else set()
+            return (is_nan, 0, 0, type_l)
         has_nan = False
         size = len(l)  # max length of lists
         depth = 1
+        types = set()
         for item in l:
-            h, s, d = _examine(item)
+            h, s, d, t = _examine(item)
             has_nan = has_nan or h
             size = max(size, s)
             depth = max(depth, d + 1)
-        return (has_nan, size, depth)
+            types.update(t)
+        return (has_nan, size, depth, types)
 
-    has_nan, size, depth = _examine(l)
-    note(f'{has_nan=}, {size=}, {depth=}')
+    has_nan, size, depth, types = _examine(l)
+    note(f'{has_nan=}, {size=}, {depth=}, {types=}')
+
+    assert len(types) <= 1  # All same type unless empty
+
+    if isinstance(dtype, np.dtype) and types:
+        type_ = next(iter(types))
+        note(f'{type_=})')
+        # TODO: Assert `dtype` matches `type_`
 
     if not allow_nan:
         assert not has_nan
@@ -93,13 +136,8 @@ def test_lists(data: st.DataObject) -> None:
 @given(data=st.data())
 def test_from_list(data: st.DataObject) -> None:
     # Draw options
-    allow_nan = data.draw(st.booleans(), label='allow_nan')
-    max_size = data.draw(st.integers(min_value=0, max_value=5), label='max_size')
-    max_depth = data.draw(st.integers(min_value=1, max_value=5), label='max_depth')
+    kwargs = data.draw(lists_kwargs(), label='kwargs')
 
     # Call the test subject
-    a = data.draw(
-        from_list(allow_nan=allow_nan, max_size=max_size, max_depth=max_depth),
-        label='a',
-    )
+    a = data.draw(from_list(**kwargs), label='a')
     assert isinstance(a, ak.Array)
