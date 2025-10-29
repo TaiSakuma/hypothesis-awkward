00f32dcfd449cdc61aebb7d55ce8b08311d5f0d9
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-29 17:45:29 -0400

    Update `simple_dtype_kinds_in()`

--- src/hypothesis_awkward/util.py
+++ src/hypothesis_awkward/util.py
@@ -3,15 +3,18 @@ import numpy as np
 
 def simple_dtype_kinds_in(d: np.dtype) -> set[str]:
     '''Kinds of simple dtypes (e.g. `i`, `f`, `M`) contained in `d`.'''
-    if d.names is None:  # simple dtype
-        kind = d.kind
-        if kind == 'V' and d.subdtype is not None:
-            kind = d.subdtype[0].kind
-        return {kind}
-    else:  # structured dtype
-        kinds = set()
-        for name in d.names:
-            f = d.fields
-            assert f is not None
-            kinds.update(simple_dtype_kinds_in(f[name][0]))
-        return kinds
+    match d.names, d.kind, d.subdtype:
+        case None, kind, None:  # simple dtype
+            return {kind}
+        case None, 'V', tuple(subdtype):  # sub-array dtype
+            # E.g., subdtype == (dtype('int32'), (3, 4))
+            return {subdtype[0].kind}
+        case tuple(names), 'V', None:  # structured dtype
+            kinds = set()
+            for name in names:
+                f = d.fields
+                assert f is not None
+                kinds.update(simple_dtype_kinds_in(f[name][0]))
+            return kinds
+        case _:
+            raise TypeError(f'Unexpected dtype: {d}')
