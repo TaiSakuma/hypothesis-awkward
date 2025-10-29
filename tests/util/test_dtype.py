d15f9d26552731d4aa419cd279dac3c8e566d068
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-29 19:40:45 -0400

    Update a test

--- tests/util/test_dtype.py
+++ tests/util/test_dtype.py
@@ -6,25 +6,22 @@ import hypothesis_awkward.strategies as st_ak
 from hypothesis_awkward.util import simple_dtype_kinds_in
 
 
-def _org_imp(d: np.dtype) -> set[str]:
-    '''Kinds of simple dtypes (e.g. `i`, `f`, `M`) contained in `d`.'''
-    if d.names is None:  # simple dtype
-        kind = d.kind
-        if kind == 'V' and d.subdtype is not None:
-            kind = d.subdtype[0].kind
-        return {kind}
-    else:  # structured dtype
-        kinds = set()
-        for name in d.names:
-            f = d.fields
-            assert f is not None
-            kinds.update(simple_dtype_kinds_in(f[name][0]))
-        return kinds
+def _is_kind_in(k: str, d: np.dtype) -> bool:
+    if (fields := d.fields) is not None:
+        return any(_is_kind_in(k, f[0]) for f in fields.values())
+    if (subdtype := d.subdtype) is not None:
+        return _is_kind_in(k, subdtype[0])
+    return d.kind == k
 
 
-@given(data=st.data())
-def test_simple_dtype_kinds_in(data: st.DataObject) -> None:
-    d = data.draw(st_ak.numpy_dtypes(), label='dtype')
-    expected = _org_imp(d)
-    actual = simple_dtype_kinds_in(d)
-    assert actual == expected
+ALL_SIMPLE_DTYPE_KINDS = {'b', 'i', 'u', 'f', 'c', 'm', 'M', 'O', 'S', 'U'}
+
+
+@given(d=st_ak.numpy_dtypes())
+def test_simple_dtype_kinds_in(d: np.dtype) -> None:
+    kinds = simple_dtype_kinds_in(d)
+    assert 'V' not in kinds
+    for k in kinds:
+        assert _is_kind_in(k, d)
+    for k in ALL_SIMPLE_DTYPE_KINDS - kinds:
+        assert not _is_kind_in(k, d)
