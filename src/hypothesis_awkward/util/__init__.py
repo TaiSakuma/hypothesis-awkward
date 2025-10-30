6e002ae1c88a42ee8b382a21fb82c8c27aea0dfa
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-30 13:53:06 -0400

    Turn a module into a package

---
+++ src/hypothesis_awkward/util/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'SUPPORTED_DTYPES',
+    'SUPPORTED_DTYPE_NAMES',
+    'simple_dtypes_in',
+    'simple_dtype_kinds_in',
+]
+
+from .dtype import (
+    SUPPORTED_DTYPE_NAMES,
+    SUPPORTED_DTYPES,
+    simple_dtype_kinds_in,
+    simple_dtypes_in,
+)
